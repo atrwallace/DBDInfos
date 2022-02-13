f5e7dfb5bb687e6eac778e7b45ab11cc128abbef
Altair Oliveira <atr.wall@gmail.com>
2022-02-12 21:32:21 -0300

    SplashActivity added, intro animation added, improvement performance on the Adapter, camel case updated.

---
+++ app/src/main/java/com/example/dbdinfos/presentation/home/SplashActivity.kt
@@ -0,0 +1,45 @@
+package com.example.dbdinfos.presentation.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import com.example.dbdinfos.databinding.ActivitySplashBinding
+import com.example.dbdinfos.presentation.login.MainActivity
+import com.example.dbdinfos.util.FirebaseConfig
+
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val auth = FirebaseConfig().fireInstance()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val handler = Handler()
+        handler.postDelayed({
+            // do something after 1000ms
+            setUpScreen()
+        }, 2000)
+    }
+
+    fun setUpScreen() {
+
+        if (auth.currentUser != null) {
+            startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+            finish()
+
+        } else {
+            val intent = Intent(this, MainActivity::class.java)
+            val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                this@SplashActivity,
+                binding.imageView2,
+                "profile"
+            )
+            startActivity(intent, options.toBundle())
+            finish()
+        }
+
+    }
+}
